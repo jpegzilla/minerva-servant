719dbc90b20c8c759806ff96d83e6845fde29914
jpegzilla <eris@jpegzilla.com>
2020-02-10 05:36:47 -0500

    ok why would I try dynamically changing the port mid run

--- src/server.js
+++ src/server.js
@@ -2,8 +2,3 @@ const app = require("./index");
 const { port } = require("./config");
 
 app.listen(port, () => console.log(`listening on ${port}.`));
-
-app.on("error", e => {
-  if (e.code.toLowerCase() === "eaddrinuse")
-    app.listen(port + 1, () => console.log(`listening on ${port + 1}`));
-});
