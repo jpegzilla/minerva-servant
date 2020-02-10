c1ceddd95b9e12f4259cf5f2fb0d07dd8722462a
jpegzilla <eris@jpegzilla.com>
2020-02-10 16:16:35 -0500

    documenting endpoints

--- src/endpoints/update.js
+++ src/endpoints/update.js
@@ -8,15 +8,18 @@ router.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 router.use(bodyParser.json());
 
+// endpoint for updating user documents in ma_users collection
 router.post("/user", async (req, res) => {
   const type = "user";
-  console.log(req.body);
+
+  // check to ensure valid body
   if (!req.body || Object.entries(req.body).length == 0) {
     return res
       .status(400)
       .json({ message: "invalid request. must send object" });
   }
 
+  // check to make sure there is a user and an object to update the user
   if (!req.body.user || !req.body.userUpdate) {
     return res.status(400).json({
       ok: false,
@@ -25,30 +28,37 @@ router.post("/user", async (req, res) => {
   }
 
   try {
-    const found = await db.find(req.body.user, type);
+    // try to find the user by their id
+    const found = await db.findById(req.body.user, type);
     if (found.length < 1)
       return res
         .status(400)
         .json({ ok: false, message: "record does not exist." });
 
+    // try to update the user with the provided object
     const op = await db.update(req.body.user, req.body.userUpdate, type);
+
     return res
       .status(200)
       .json({ ok: true, message: `${op.insertedCount} inserted.` });
   } catch (err) {
+    // return error if either db method above fails
     return res.status(500).json({ ok: false, message: err });
   }
 });
 
+// try to update a document in the ma_data collection
 router.post("/data", async (req, res) => {
   const type = "data";
-  console.log(req.body);
+
+  // validate request body
   if (!req.body || Object.entries(req.body).length == 0) {
     return res
       .status(400)
       .json({ message: "invalid request. must send object" });
   }
 
+  // ensure request body structure
   if (!req.body.item || !req.body.itemUpdate) {
     return res.status(400).json({
       ok: false,
@@ -57,17 +67,21 @@ router.post("/data", async (req, res) => {
   }
 
   try {
-    const found = await db.find(req.body.item, type);
+    // find the document by id
+    const found = await db.findById(req.body.item, type);
     if (found.length < 1)
       return res
         .status(400)
         .json({ ok: false, message: "record does not exist." });
 
+    // update the document with provided update object
     const op = await db.update(req.body.item, req.body.userUpdate, type);
+
     return res
       .status(200)
       .json({ ok: true, message: `${op.insertedCount} inserted.` });
   } catch (err) {
+    // if either db method fails, return error
     return res.status(500).json({ ok: false, message: err });
   }
 });
