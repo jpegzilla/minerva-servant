c3e8c27bec1c2efa7d3620656f977b33d6652edb
jpegzilla <eris@jpegzilla.com>
2020-05-07 04:55:06 -0400

    removed javascript implementation, rewriting in ruby

--- src/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  port: process.env.PORT || 8080,
-  env: process.env.ENV || "dev",
-  key: process.env.ADMINKEY || null
-};
--- src/server.js
+++
@@ -1,4 +0,0 @@
-const app = require("./index");
-const { port } = require("./config");
-
-app.listen(port, () => console.log(`listening on ${port}.`));
