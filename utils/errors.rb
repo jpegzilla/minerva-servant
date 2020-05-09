d31772486a351661e5bfcfa89b64a41bd26f49fe
jpegzilla <eris@jpegzilla.com>
2020-05-09 19:26:04 -0400

    added status codes to all errors

--- utils/errors.rb
+++ utils/errors.rb
@@ -3,27 +3,45 @@
 # simple methods for showing error messages
 module ErrorMessage
   def self.invalid_key_error(method, key)
-    raise ArgumentError, "invalid key '#{key}' used as parameter to #{method}."
+    raise ArgumentError, {
+      message: "invalid key '#{key}' used as parameter to #{method}."
+    }.to_json
   end
 
   def self.invalid_http_method_error(key, val, method)
-    raise ArgumentError, "#{key} used to access #{method}. use #{val}."
+    raise ArgumentError, {
+      message: "#{key} used to access #{method}. use #{val}.",
+      status: 405,
+      options: {
+        allowed: val
+      }
+    }.to_json
   end
 
   def self.missing_arguments_error(method)
-    raise ArgumentError, "missing arguments to #{method}"
+    raise ArgumentError, {
+      status: 500,
+      message: "missing arguments to #{method}"
+    }.to_json
   end
 
   def self.missing_parameter_error(method)
-    raise ArgumentError, "missing url parameter id to #{method}."
+    raise ArgumentError, {
+      status: 400,
+      message: "missing url parameter id to #{method}."
+    }.to_json
   end
 
   def self.invalid_structure_error(method, keys)
-    raise ArgumentError, "invalid object with keys #{keys} passed to #{method}."
+    raise ArgumentError, {
+      status: 400,
+      message: "invalid object with keys #{keys} passed to #{method}."
+    }.to_json
   end
 
-  def self.make_http_error(status, message)
-    error = { status: status }
+  def self.make_http_error(status, message, options)
+    error = { status: status, options: options }
+    error.delete :options if options.nil?
 
     case status.to_s.chr.to_i
     when 4
