5ff5b4863aafb34b8829d56dcf8e0f3fb1a9f527
jpegzilla <eris@jpegzilla.com>
2020-05-08 21:13:45 -0400

    starts in development mode with --development

--- main.rb
+++ main.rb
@@ -2,6 +2,12 @@
 
 require_relative 'server'
 
+$PROCESS_MODE = 'production'
+
+ARGV.each do |arg|
+  $PROCESS_MODE = 'development' if arg == '--development'
+end
+
 server = Server::HTTPServer.new(8888)
 
 server.listen
